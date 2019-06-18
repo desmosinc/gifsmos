88d1a8c859f2f80d96ecafd52827f0d1dc422094
danieljaeim <imdanieljae@gmail.com>
2019-06-18 16:14:28 -0700

    added functionality to click left and right through frames

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import classNames from 'classnames';
 import Frame from './Frame';
 import './Preview.css';
+import left from './icons/left.svg';
+import right from './icons/right.svg';
 
 class Preview extends Component {
   constructor(props) {
@@ -10,6 +12,7 @@ class Preview extends Component {
     this.handleGenerateGIF = this.handleGenerateGIF.bind(this);
     this.handleTogglePlaying = this.handleTogglePlaying.bind(this);
     this.handleDeleteFrame = this.handleDeleteFrame.bind(this);
+    this.handleIncrementPreviewIdx = this.handleIncrementPreviewIdx.bind(this);
   }
 
   handlePreviewUpdate(evt) {
@@ -61,6 +64,19 @@ class Preview extends Component {
     updatePreviewIdx(newIdx);
   }
 
+  handleIncrementPreviewIdx(incrementUp) {
+    const { previewIdx, frameIDs, updatePreviewIdx } = this.props;
+
+    if (!!incrementUp) {
+      const newIdx =
+        previewIdx + 1 > frameIDs.length - 1 ? previewIdx : previewIdx + 1;
+      updatePreviewIdx(newIdx);
+    } else {
+      const newIdx = previewIdx - 1 < 0 ? 0 : previewIdx - 1;
+      updatePreviewIdx(newIdx);
+    }
+  }
+
   render() {
     const {
       expanded,
@@ -77,11 +93,24 @@ class Preview extends Component {
 
     return (
       <div className={classNames('Preview', { 'Preview-expanded': expanded })}>
-        <Frame
-          imageSrc={imageSrc}
-          playing={playing}
-          togglePlaying={this.handleTogglePlaying}
-        />
+        <div className="Frame-section-container">
+          <img
+            className="directional-icon"
+            src={left}
+            onClick={() => this.handleIncrementPreviewIdx(false)}
+          />
+          <Frame
+            imageSrc={imageSrc}
+            playing={playing}
+            togglePlaying={this.handleTogglePlaying}
+          />
+          <img
+            className="directional-icon"
+            src={right}
+            onClick={() => this.handleIncrementPreviewIdx(true)}
+          />
+        </div>
+
         <div className="Preview-scrubber">
           <input
             type="range"
@@ -93,6 +122,7 @@ class Preview extends Component {
             aria-label="preview frame index"
           />
         </div>
+
         <div className="Preview-scrubber-counter">
           {numFrames ? `${previewIdx + 1} / ${numFrames}` : '0 / 0'}
         </div>
@@ -120,6 +150,7 @@ class Preview extends Component {
             </button>
           )}
         </div>
+
         <div className="Preview-progress-outer">
           <div
             className="Preview-progress-inner"
