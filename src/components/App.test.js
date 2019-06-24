ac0614189f15683b68f410a29098c54da130c75d
danieljaeim <imdanieljae@gmail.com>
2019-06-24 11:49:53 -0700

    delete .env and fix app tests

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -6,7 +6,6 @@ afterEach(cleanup);
 
 describe('<App/>', () => {
   it('renders without crashing', () => {
-    let wrapper = global.renderWithRedux(<App />);
     global.renderWithRedux(<App />);
   });
 });
