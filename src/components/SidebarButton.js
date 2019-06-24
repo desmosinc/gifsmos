e1793a03a67765e0fb8ad50d62d7dc85d863cb20
danieljaeim <imdanieljae@gmail.com>
2019-06-24 14:21:55 -0700

    edit hover styling as per p.request

--- src/components/SidebarButton.js
+++ src/components/SidebarButton.js
@@ -27,18 +27,21 @@ const ariaMap = {
 };
 
 const SidebarButton = ({ children, icon, onClick, expanded }) => (
-  <button
-    className={classNames('SidebarButton', {
-      'SidebarButton-expanded': expanded
-    })}
-    onClick={onClick}
-    aria-label={ariaMap[icon]}
-    aria-expanded={expanded}
-    data-testid={`SidebarButton-${icon}-button`}
-  >
-    <img src={iconMap[icon]} alt={`${icon} icon`} />
-    {children}
-  </button>
+  <div className="tooltip">
+    <span className="tooltiptext">{icon[0].toUpperCase() + icon.slice(1)}</span>
+    <button
+      className={classNames('SidebarButton', {
+        'SidebarButton-expanded': expanded
+      })}
+      onClick={onClick}
+      aria-label={ariaMap[icon]}
+      aria-expanded={expanded}
+      data-testid={`SidebarButton-${icon}-button`}
+    >
+      <img src={iconMap[icon]} alt={`${icon} icon`} />
+      {children}
+    </button>
+  </div>
 );
 
 SidebarButton.defaultProps = {
