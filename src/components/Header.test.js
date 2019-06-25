83321d6ee1003dbbd5b0e7bd5b2e06a7d4f04da7
Spencer Phillipson <phillipson808@gmail.com>
2019-06-25 09:57:45 -0700

    preparing for merge

---
+++ src/components/Header.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import { cleanup } from '@testing-library/react';
+
+afterEach(cleanup);
+
+describe('<Header/>', () => {
+  it('renders without crashing', () => {
+    global.renderWithRedux(<Header />);
+  });
+
+  it('renders appropriate content', () => {
+    const { getByTestId } = global.renderWithRedux(<Header />);
+    expect(getByTestId('Header-logo').textContent).toBe('GIFsmos');
+    expect(getByTestId('Header-help').textContent).toMatch('Paste');
+  });
+});
