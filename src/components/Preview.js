a4d993ac41a27276d2a8ae3c7b9b1e24a6ea58dd
Billy <cole.billys@gmail.com>
2019-07-02 19:57:59 -0700

    remove duplicate settings elements, added mute preview functionality back in

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -141,84 +141,92 @@ class Preview extends Component {
           <h2>Preview</h2>
           <InfoIcon infoText={previewText} />
         </div>
-        <div className="Frame-section-container">
-          <img
-            className="Frame-directional-icon"
-            src={left}
-            onClick={() =>
-              this.handleChangePreviewIdx(Math.max(previewIdx - 1, 0))
-            }
-            alt=""
-          />
-          <Frame
-            imageSrc={imageSrc}
-            playing={playing}
-            togglePlaying={this.handleTogglePlaying}
-            caption={caption}
-            fontColor={fontColor}
-            textPosition={textPosition}
-          />
-          <img
-            className="Frame-directional-icon"
-            src={right}
-            onClick={() =>
-              this.handleChangePreviewIdx(
-                Math.min(previewIdx + 1, frameIDs.length - 1)
-              )
-            }
-            alt=""
-          />
-        </div>
-        <div className="Preview-scrubber" data-testid="Preview-scrubber">
-          <input
-            type="range"
-            min="0"
-            max={numFrames - 1}
-            value={previewIdx}
-            onChange={this.handlePreviewUpdate}
-            disabled={!numFrames}
-            aria-label="preview frame index"
-          />
-          <div
-            className="Preview-scrubber-counter"
-            data-testid="Preview-scrubber-counter"
-          >
-            {numFrames ? `${previewIdx + 1} / ${numFrames}` : '0 / 0'}
+        <div className={classNames({ 'Preview-muted': !numFrames })}>
+          <div className="Frame-section-container">
+            <img
+              className="Frame-directional-icon"
+              src={left}
+              onClick={() =>
+                this.handleChangePreviewIdx(Math.max(previewIdx - 1, 0))
+              }
+              alt=""
+            />
+            <Frame
+              imageSrc={imageSrc}
+              playing={playing}
+              togglePlaying={this.handleTogglePlaying}
+              caption={caption}
+              fontColor={fontColor}
+              textPosition={textPosition}
+            />
+            <img
+              className="Frame-directional-icon"
+              src={right}
+              onClick={() =>
+                this.handleChangePreviewIdx(
+                  Math.min(previewIdx + 1, frameIDs.length - 1)
+                )
+              }
+              alt=""
+            />
+          </div>
+          <div className="Preview-scrubber" data-testid="Preview-scrubber">
+            <input
+              type="range"
+              min="0"
+              max={numFrames - 1}
+              value={previewIdx}
+              onChange={this.handlePreviewUpdate}
+              disabled={!numFrames}
+              aria-label="preview frame index"
+            />
+            <div
+              className="Preview-scrubber-counter"
+              data-testid="Preview-scrubber-counter"
+            >
+              {numFrames ? `${previewIdx + 1} / ${numFrames}` : '0 / 0'}
+            </div>
+          </div>
+          <div className="Frame-delete">
+            {numFrames ? (
+              <button
+                className="Frame-delete-redo-button"
+                aria-label="delete this frame"
+                onClick={() => this.handleDeleteFrame(previewIdx)}
+              >
+                Delete this Frame
+              </button>
+            ) : null}
+            {redoFrames.length ? (
+              <button
+                className="Frame-delete-redo-button"
+                aria-label="redo last frame"
+                onClick={this.handleRedoFrame}
+              >
+                Undo Delete
+              </button>
+            ) : null}
+          </div>
+          <div className="Frame-timeline">
+            {frameIDs.map((frameID, i) => (
+              <img
+                className={classNames('Frame-scroll', {
+                  'Frame-scroll-active': previewIdx === i
+                })}
+                src={frames[frameID]}
+                onClick={() => this.handleChangePreviewIdx(i)}
+                alt=""
+                key={`frame-id-${frameID}`}
+              />
+            ))}
           </div>
         </div>
-        <div className="Frame-delete">
-          {numFrames ? (
-            <button
-              className="Frame-delete-redo-button"
-              aria-label="delete this frame"
-              onClick={() => this.handleDeleteFrame(previewIdx)}
-            >
-              Delete this Frame
-            </button>
-          ) : null}
-          {redoFrames.length ? (
-            <button
-              className="Frame-delete-redo-button"
-              aria-label="redo last frame"
-              onClick={this.handleRedoFrame}
-            >
-              Undo Delete
-            </button>
-          ) : null}
-        </div>
-        <div className="Frame-timeline">
-          {frameIDs.map((frameID, i) => (
-            <img
-              className={classNames('Frame-scroll', {
-                'Frame-scroll-active': previewIdx === i
-              })}
-              src={frames[frameID]}
-              onClick={() => this.handleChangePreviewIdx(i)}
-              alt=""
-              key={`frame-id-${frameID}`}
-            />
-          ))}
-        </div>
+        {!numFrames ? (
+          <div className="Preview-no-frames">
+            No frames have been captured. Use the camera or burst tools to add
+            some!
+          </div>
+        ) : null}
         <div className="Preview-create">
           {!!numFrames && this.props.gifData.length === 0 ? (
             <GenerateGifFormContainer
@@ -239,12 +247,6 @@ class Preview extends Component {
         </div>
         {gifProgress === 1 ? (
           <div className="Preview-progress-success">Download Successful </div>
-        ) : null}
-        {!numFrames ? (
-          <div className="Preview-no-frames">
-            No frames have been captured. Use the camera or burst tools to add
-            some!
-          </div>
         ) : null}
       </div>
     );
